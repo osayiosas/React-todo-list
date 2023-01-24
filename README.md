34d51c216eccf4e3ab5b0164228caf0534d0606f
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-01-24 17:15:44 +0100

    working on CRUD React todo-list

---
+++ README.md
@@ -0,0 +1,3 @@
+# Working on a todo list with React 
+
+learning how to use  Crude in React.
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import { useState } from "react";
+
+
+function App()
+{
+
+  const [todoList, setTodoList] = useState([]);
+  
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) =>
+  {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () =>
+  {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length -1].id + 1,
+      taskName: newTask
+    }
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) =>
+  {
+    setTodoList(todoList.filter((task) => task.id !== id));
+
+  }
+
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange}/>
+        <button  onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) =>
+        {
+          return (
+            <div>
+              <h1>{task.taskName}</h1>
+              <button onClick={() => deleteTask(task.id)}>x</button>
+            </div>
+          );
+        })}
+      </div>
+      
+    </div>
+  )
+}
+
+
+export default App;
