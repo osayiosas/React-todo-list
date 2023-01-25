825971d47c4879cc19b0d250d3ff802ad87d3a3e
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-01-25 12:06:04 +0100

    added more functions

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Working on a todo list with React 
 
 learning how to use  Crude in React.
+
+
+# Add more functionality to the TodoList 
+
+made a it have a complete task button using .map and selecting through literal notation with the key value pair.
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import {Task} from "./Task"
 
 
 function App()
@@ -18,7 +19,8 @@ function App()
   {
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length -1].id + 1,
-      taskName: newTask
+      taskName: newTask,
+      completed: false,
     }
     setTodoList([...todoList, task]);
   };
@@ -27,6 +29,20 @@ function App()
   {
     setTodoList(todoList.filter((task) => task.id !== id));
 
+  }
+
+  const completeTask = (id) =>
+  {
+    setTodoList(todoList.map((task) =>
+    {
+      if (task.id === id) {
+        return { ...task, completed: true };
+      } else {
+        return task;
+      }
+    }
+
+    ))
   }
 
   return (
@@ -38,12 +54,10 @@ function App()
       <div className="list">
         {todoList.map((task) =>
         {
-          return (
-            <div>
-              <h1>{task.taskName}</h1>
-              <button onClick={() => deleteTask(task.id)}>x</button>
-            </div>
-          );
+          return <Task taskName={task.taskName} id={task.id}
+            completed={task.completed}
+            deleteTask={deleteTask}
+            completeTask={completeTask} />;
         })}
       </div>
       
